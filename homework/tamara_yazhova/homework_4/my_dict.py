00141fdacb06f4d555cded99295f511b8ac096a3
tamarayaz <tamara90gomel@gmail.com>
2025-01-16 19:26:49 -0600

    homework_4 done and fixed Lint

--- homework/tamara_yazhova/homework_4/my_dict.py
+++ homework/tamara_yazhova/homework_4/my_dict.py
@@ -1,5 +1,5 @@
 my_dict = {
-    "tuple": (1, 2, 3 ,4 , 5),
+    "tuple": (1, 2, 3, 4, 5),
     "list": ["one", "two", "three", "four", "five"],
     "dict": {"name": "Kate", "surname": "Watson", "age": 35, "job:": "doctor", "city": "Texas"},
     "set": {100, "red", "text", 50, "green"}
@@ -8,9 +8,8 @@ my_dict = {
 print(my_dict["tuple"][-1])
 my_dict["list"].append('six')
 my_dict["list"].pop(1)
-my_dict["dict"][("i'm a tuple")] = "new"
+my_dict["dict"]["i'm a tuple"] = "new"
 my_dict["dict"].pop("age")
 my_dict["set"].add(3000)
 my_dict["set"].discard("text")
 print(my_dict)
-
