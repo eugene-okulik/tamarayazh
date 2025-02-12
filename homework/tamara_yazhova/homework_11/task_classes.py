8dc49d426aa8a20d79492c0722ae64e47627427d
tamarayaz <tamara90gomel@gmail.com>
2025-02-11 18:34:36 -0600

    fixed formatting

--- homework/tamara_yazhova/homework_11/task_classes.py
+++ homework/tamara_yazhova/homework_11/task_classes.py
@@ -19,7 +19,7 @@ class Book:
 
 
 class Textbook(Book):
-    def __init__(self, name, author, page_count, isbn, subject, group, task=True, reserve = False):
+    def __init__(self, name, author, page_count, isbn, subject, group, task=True, reserve=False):
         super().__init__(name, author, page_count, isbn, reserve)
         self.subject = subject
         self.group = group
@@ -33,11 +33,12 @@ class Textbook(Book):
             return (f"Название: {self.name}, Автор: {self.author}, "
                     f"страниц: {self.page_count}, предмет: {self.subject}, класс: {self.group}")
 
+
 book_1 = Book("Идиот", "Достоевский", 500, 687698)
 book_2 = Book("Фауст", "Гете", 456, 573485)
 book_3 = Book("Сто лет одиночества", "Маркес", 645, 563789)
 book_4 = Book("Герой нашего времени", "Лермонтов", 256, 545123)
-book_5 = Book("Мастер и Маргарита", "Булгаков", 567, 658456, reserve = True)
+book_5 = Book("Мастер и Маргарита", "Булгаков", 567, 658456, reserve=True)
 
 books = [book_1, book_2, book_3, book_4, book_5]
 
@@ -56,7 +57,7 @@ text_book_3 = Textbook(
     "Литература", 7, False, reserve=True
 )
 text_book_4 = Textbook(
-    "Геометрия", "Борисова",78, 567587, "Математика", 7, True
+    "Геометрия", "Борисова", 8, 567587, "Математика", 7, True
 )
 
 textbooks = [text_book_1, text_book_2, text_book_3, text_book_4]
