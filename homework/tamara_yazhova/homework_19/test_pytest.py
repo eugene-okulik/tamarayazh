72f29d5e6ca6b580964f0d86db5f10d19a4d77f6
tamarayaz <tamara90gomel@gmail.com>
2025-03-26 18:14:32 -0600

    fixed formatting

--- homework/tamara_yazhova/homework_19/test_pytest.py
+++ homework/tamara_yazhova/homework_19/test_pytest.py
@@ -12,11 +12,13 @@ def add_messages_for_all():
     yield
     print("\nTesting completed")
 
+
 @pytest.fixture()
 def add_message_for_each():
     print("\nbefore test")
     yield
     print("\nafter test")
+
 
 @pytest.fixture()
 def create_object():
@@ -26,11 +28,13 @@ def create_object():
     yield post_id
     requests.delete(f'{BASE_URL}/{post_id}')
 
+
 @pytest.mark.parametrize("name, data", [
     ("Object_1", {"color": "red", "size": "small"}),
     ("Object_2", {"color": "white", "size": "small"}),
     ("Object_3", {"color": "black", "size": "big"}),
 ])
+
 
 @pytest.mark.critical
 def test_create_post(name, data, add_messages_for_all, add_message_for_each):
@@ -40,10 +44,12 @@ def test_create_post(name, data, add_messages_for_all, add_message_for_each):
     obj_id = response.json()['id']
     requests.delete(f'{BASE_URL}/{obj_id}')
 
+
 @pytest.mark.medium
 def test_get_all_posts(add_message_for_each):
     response = requests.get(BASE_URL)
     assert response.status_code == 200, f"Failed to get posts: {response.status_code}, {response.text}"
+
 
 def test_update_post_put(create_object, add_message_for_each):
     post_id = create_object
@@ -58,12 +64,14 @@ def test_update_post_put(create_object, add_message_for_each):
         f'{BASE_URL}/{post_id}', json=body, headers=HEADERS).json()
     assert response['name'] == 'Pulp Fiction'
 
+
 def test_update_post_patch(create_object, add_message_for_each):
     post_id = create_object
     body = {"name": "Pulp Fiction-UPD"}
     response = requests.patch(
         f'{BASE_URL}/{post_id}', json=body, headers=HEADERS).json()
     assert response['name'] == 'Pulp Fiction-UPD', f"Failed to update post: {response}"
+
 
 def test_delete_post(create_object, add_message_for_each):
     post_id = create_object
