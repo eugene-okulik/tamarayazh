34171b2b3c2873f102b6c94ff954bb2b1b5144ea
tamarayaz <tamara90gomel@gmail.com>
2025-04-08 18:10:35 -0600

    fixed formatting

--- homework/tamara_yazhova/homework_22/locustfile.py
+++ homework/tamara_yazhova/homework_22/locustfile.py
@@ -5,7 +5,6 @@ class ObjectUser(HttpUser):
     id = None
     headers = {"Content-type": "application/json"}
 
-
     def on_start(self):
         body = {
             "name": "Second object",
@@ -17,16 +16,13 @@ class ObjectUser(HttpUser):
         response = self.client.post('/object', json=body, headers=self.headers)
         self.id = response.json().get('id')
 
-
     @task(1)
     def get_all_objects(self):
         self.client.get('/object', headers=self.headers)
 
-
     @task(3)
     def get_one_object(self):
         self.client.get(f'/object/{self.id}', headers=self.headers)
-
 
     def on_stop(self):
         self.client.delete(f'/object/{self.id}', headers=self.headers)
