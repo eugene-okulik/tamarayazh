f05493f97b179f2fa8da35bfc4e7c71856348f28
tamarayaz <tamara90gomel@gmail.com>
2025-06-12 17:18:49 -0600

    homework_32 done

---
+++ homework/tamara_yazhova/homework_32/main.py
@@ -0,0 +1,13 @@
+import requests
+from datetime import datetime
+
+
+def get_weather():
+    now = datetime.now()
+    date_time = now.strftime("%d %B %H:%M")
+    response  = requests.get("https://wttr.in/Minsk?format=%t")
+    temperature = response.text
+    print(f"The weather in minsk {date_time}:")
+    print(temperature)
+
+get_weather()
